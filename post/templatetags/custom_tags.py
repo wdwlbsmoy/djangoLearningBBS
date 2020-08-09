baad8023bead4ad09454af9bcefdf708060fdfc9
John xue <542530488@qq.com>
2020-08-09 08:05:06 +0800

    模板修改學習

--- post/templatetags/custom_tags.py
+++ post/templatetags/custom_tags.py
@@ -9,3 +9,15 @@ def prefix_tag(cur_str):
 @register.inclusion_tag('post/inclusion.html',takes_context=True)
 def hello_inclusion_tag(context,cur_str):
     return {'hello':'%s %s' %(context['prefix'],cur_str)}
+
+@register.simple_tag
+def hello_assignment_tag(cur_str):
+    return 'Hello: %s' % cur_str
+
+# @register.filter
+# def replace_django(value):
+#     return value.replace('django','Django')
+
+@register.filter(name='r_django')
+def replace_django(value,base):
+    return value.replace('django',base)
